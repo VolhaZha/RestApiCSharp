f2e0ab2b3f318ffe6bd05740059450d452f477ee
VolhaZha <olgazhauryd@gmail.com>
2025-10-19 17:01:58 +0300

    Task 10

---
+++ Authentication/Authenticator.cs
@@ -0,0 +1,53 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System.Text.Json.Serialization;
+
+namespace RestApiCSharp.Authentication
+{
+    public class Authenticator : AuthenticatorBase
+    {
+        record TokenResponse
+        {
+            [JsonPropertyName("token_type")]
+            public string TokenType { get; init; }
+            [JsonPropertyName("access_token")]
+            public string AccessToken { get; init; }
+        }
+
+        readonly string _baseUrl;
+        readonly string _clientId;
+        readonly string _clientSecret;
+        readonly string _scope;
+
+        public Authenticator(string baseUrl, string clientId, string clientSecret, string scope) : base("")
+        {
+            _baseUrl = baseUrl;
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _scope = scope;
+        }
+
+        protected override async ValueTask<Parameter> GetAuthenticationParameter(string accessToken)
+        {
+            Token = string.IsNullOrEmpty(Token) ? await GetToken() : Token;
+            return new HeaderParameter(KnownHeaders.Authorization, Token);
+        }
+
+        async Task<string> GetToken()
+        {
+            var options = new RestClientOptions(_baseUrl)
+            {
+                Authenticator = new HttpBasicAuthenticator(_clientId, _clientSecret),
+            };
+
+            using var client = new RestClient(options);
+
+            var request = new RestRequest("oauth/token")
+                .AddParameter("grant_type", "client_credentials")
+                .AddParameter("scope", _scope);
+            var response = await client.PostAsync<TokenResponse>(request);
+
+            return $"{response!.TokenType} {response!.AccessToken}";
+        }
+    }
+}
