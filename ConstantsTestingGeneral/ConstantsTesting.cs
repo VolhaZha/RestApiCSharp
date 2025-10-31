eb11b929bf701d7509ea456a5f3b7375853d642e
VolhaZha <olgazhauryd@gmail.com>
2025-10-31 08:47:13 +0300

    Task 20

---
+++ ConstantsTestingGeneral/ConstantsTesting.cs
@@ -0,0 +1,11 @@
+﻿namespace RestApiCSharp.ConstantsTestingGeneral
+{
+    public static class ConstantsTesting
+    {
+        public const string BaseUrl = "http://localhost:5050";
+        public const string ClientId = "0oa157tvtugfFXEhU4x7";
+        public const string ClientSecret = "X7eBCXqlFC7x-mjxG5H91IRv_Bqe1oq7ZwXNA8aq";
+        public const string ReadScope = "read";
+        public const string WriteScope = "write";
+    }
+}
